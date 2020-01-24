4311e45457d33a89c745385eb0c8cc518151f642
Tim Cheah <cheah.timothy@gmail.com>
2020-01-23 19:08:09 -0600

    a

--- IntegerSequence/main.cpp
+++ IntegerSequence/main.cpp
@@ -2,31 +2,28 @@
 
 using namespace std;
 
-int main()
-{
+// Formula: Xn = X(n-1) + X(n-2)
+// for n>1
 
+int number(int x) {
 
-    int sequences;
-    int number1 = 0;
-    int number2;
-    int total = 0;
-
-		for (int n = 1; n <= sequences; ++n) {
-			// save number1 to the input
-			number1 = inputFile.nextInt();
-			// save number2 to the input
-			number2 = inputFile.nextInt();
-			// loop number1 to number2
-			for (int n1 = number1; n1 <= number2; n1++) {
-				// OUTPUT
-				// find the total of the loop
-				total = n1 + total;
-			}
-			// display output
-			outputFile.println(total);
-			// reset total to "0"
-			total = 0;
-		}
-    cout << "Hello world!" << endl;
-    return 0;
+   if( x == 0) {
+      return(x);
+   }else {
+      return(number(x-1) + number(x-2));
+   }
 }
+
+int main() {
+   int x , i = 0;
+   cout << "How large(in numbers) would you like your sequence to be? ";
+   cin >> x;
+   cout << "The sequence : ";
+
+   while(i < x) {
+      cout << " " << number(i);
+      i++;
+   }
+   return 0;
+}
+
