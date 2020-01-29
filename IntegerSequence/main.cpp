883187bbb43e9d2eb11f3cf8420254783d421ed0
Tim Cheah <cheah.timothy@gmail.com>
2020-01-29 14:23:30 -0600

    update

--- IntegerSequence/main.cpp
+++ IntegerSequence/main.cpp
@@ -6,24 +6,23 @@ using namespace std;
 // for n>1
 
 int number(int x) {
-
-   if( x == 0) {
+   if((x==1)||(x==0)) {
       return(x);
    }else {
-      return(number(x-1) + number(x-2));
+      return(number(x-1)+number(x-2));
    }
 }
 
 int main() {
-   int x , i = 0;
+   int x , i=0;
    cout << "How large(in numbers) would you like your sequence to be? ";
    cin >> x;
    cout << "The sequence : ";
-
    while(i < x) {
       cout << " " << number(i);
       i++;
    }
    return 0;
 }
+
 
