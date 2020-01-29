e17c8e1b9066040c9c4531a94e40f6f6ae2597d8
Tim Cheah <cheah.timothy@gmail.com>
2020-01-29 10:58:46 -0600

    update CircuitOO

--- CircuitOO/main.cpp
+++ CircuitOO/main.cpp
@@ -27,19 +27,29 @@ int main()
     cin >> Resistance;
     myCircuit.SetResistance(Resistance);
     cout << myCircuit.calculateVoltage() << endl;
+
+    // Calculate Amperage
     } else if (userChoice == 2) {
+    cout << "Let's calculate the amperage!" << endl;
+    cout << "What is the Voltage?" << endl;
+    cin >> Voltage;
+    myCircuit.SetVoltage(Voltage);
+    cout << "What is the Resistance?" << endl;
+    cin >> Resistance;
+    myCircuit.SetResistance(Resistance);
+    cout << myCircuit.calculateAmperage() << endl;
 
+    // Calculate Resistance
     } else if (userChoice == 3) {
-
+    cout << "Let's calculate the resistance!" << endl;
+    cout << "What is the Voltage?" << endl;
+    cin >> Voltage;
+    myCircuit.SetVoltage(Voltage);
+    cout << "What is the Amperage?" << endl;
+    cin >> Amperage;
+    myCircuit.SetAmperage(Amperage);
+    cout << myCircuit.calculateResistance() << endl;
     }
-
-
-    //cout << "Please enter voltage" << endl;
-    //cin >> Voltage;
-    //myCircuit.SetVoltage(Voltage);
-
-    //cout << myCircuit.GetVoltage() << endl;
-
 
 
 }
