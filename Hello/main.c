ad02c6f9318358370643e7f2e46fbf685b4c3798
Tim Cheah <cheah.timothy@gmail.com>
2020-02-26 14:49:54 -0600

    update Hello

--- Hello/main.c
+++ Hello/main.c
@@ -4,6 +4,18 @@
 // * = value of
 // & = memory of
 
+void getVoltage (double amperage, double resistance, double* voltage){
+    *voltage = amperage * resistance;
+}
+
+void getAmperage (double* amperage, double resistance, double voltage) {
+    *amperage = voltage / resistance;
+}
+
+void getResistance (double amperage, double* resistance, double voltage) {
+    *resistance = voltage / amperage;
+}
+
 void getResult (double parameter, int* resultCode, double* value) {
 // do omething complicated and return a code and message
 // return an int and a string in Java
@@ -13,18 +25,27 @@ void getResult (double parameter, int* resultCode, double* value) {
 }
 
 
-
 double square (double* valueToSquare) {
 *valueToSquare *= *valueToSquare;
 return *valueToSquare;
 }
 
 int main() {
+double amperage = 10;
+double resistance = 5, voltage;
+
+getVoltage(amperage, resistance, &voltage);
+printf("The voltage is %lf", voltage);
+printf("Resistance is %lf", resistance);
+
+
+/*
 int result;
 double value;
 getResult(5,&result,&value);
 printf("This code is %d and the answer is %lf\n",result,value);
 return 0;
+*/
 
 /*
 double value = 5;
