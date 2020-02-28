3230a3755be2ac74ee26e25945e5bca514d188c8
Tim Cheah <cheah.timothy@gmail.com>
2020-02-28 14:36:42 -0600

    update Hello

--- Hello/main.c
+++ Hello/main.c
@@ -69,50 +69,7 @@ getResistance(amperage, &resistance, voltage);
 printf("The resistace is %lf", resistance);
 
 }
-/*
-printf("Please enter the voltage");
-scanf("%lf", &voltage);
-printf("The voltage is %lf", voltage);
 
-
-getVoltage(amperage, resistance, &voltage);
-printf("The voltage is %lf", voltage);
-printf("Resistance is %lf", resistance);
-
-
-
-int result;
-double value;
-getResult(5,&result,&value);
-printf("This code is %d and the answer is %lf\n",result,value);
-return 0;
-*/
-
-/*
-double value = 5;
-square(&value);
-
-printf("The value is %1f\n",value);
-
-return 0;
-*/
-/*
-    int phillip = 5; //0101
-    int chad = 13;   //1101
-    int a = 5;
-    int b = 10;
-    int c = 11;
-    // int pointer
-    int x = &a;
-    int y = &b;
-    int z = &c;
-
-
-    printf("x = 0x%x and y = 0x%x and z = 0x%x and a = %d and b = %d and c = %d\n\n" ,x,y,z,a,b,c);
-
-    //printf("the bitwise and is %d\n", phillip & chad);
-    //scanf();
-*/
 
 
 }
