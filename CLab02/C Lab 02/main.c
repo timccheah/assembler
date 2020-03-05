69417743952d99cacbcfb1efb422222c8830ebf3
timccheah <56974539+timccheah@users.noreply.github.com>
2020-03-05 13:48:03 -0600

    Update

--- CLab02/C Lab 02/main.c
+++ CLab02/C Lab 02/main.c
@@ -1,51 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
-void CalculateTotal(int x){
-    if ((x==1)||(x==0)) {
-        return(x);
-    } else {
-    return (number(&x - 1)+number(&x - 2));
+
+int CalculateTotal(int);
+
+int CalculateTotal(int x){
+  if (n == 0 || n == 1)
+    return n;
+  else
+    return (f(n-1) + f(n-2));
     }
 
 }
 
 int main() {
-    int x;
-    int i = 0;
+  int n, i = 0, c;
+  printf("Enter the number of terms: ");
+  scanf(" %d", &n);
+  printf("Integer Sequence:\n");
 
-    printf("How large(in numbers) would you like your sequence to be?\n");
-    scanf("%lf", &x);
-    printf("The sequence :  ");
-    while(i < x) {
-        printf(number(i));
-        i++;
-    }
-    return 0;
-*/
-//    int* myPointer = malloc(sizeof(int));
-//    free(myPointer)
+  for (c = 1; c <= n; c++)  {
+    printf("  %d", f(i));
+    i++;
+  }
 
-void CalculateTotal(double sequence, double* total){
-    if((sequence==1)||(sequence==0)) {
-      return(0);
-   }else {
-//    printf(CalculateTotal(sequence-1)+CalculateTotal(sequence-2));
-//      *total = CalculateTotal(*sequence-1)+CalculateTotal(*sequence-2);
-   }
+  return 0;
 }
-
-int main(){
-    double total;
-    double sequence;
-    double i = 0;
-    printf("How large(in numbers) would you like your sequence to be?\n");
-    scanf("%lf", &sequence);
-    printf("%lf", sequence);
-    while (i<sequence){
-        CalculateTotal(sequence, &total);
-    }
-
-    printf("%lf", total);
-}
-
